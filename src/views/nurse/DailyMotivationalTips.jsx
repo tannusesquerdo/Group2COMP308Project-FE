e02cb09705da645a85c4bb522b775f4edad8a880
Tarique Jemison <jemisontarique@gmail.com>
2023-12-11 17:17:20 -0500

    updated dailymotivational page

--- src/views/nurse/DailyMotivationalTips.jsx
+++ src/views/nurse/DailyMotivationalTips.jsx
@@ -1,11 +1,3 @@
-/**
- * Last Modified by T. Jemison on 12/04/2023
- * Component: DailyMotivationalTips
- * Allows nurses to enter and store motivational tips for patients.
- * Nurses can add tips which are stored in the database for daily viewing by patients.
- * Props: 
- *  - saveMotivationalTips: Function to handle saving of motivational tips to the database.
- */
 import React, { useState } from 'react';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
@@ -14,18 +6,24 @@ import { toast } from 'react-toastify';
 
 function DailyMotivationalTips() {
   const [motivationalTip, setMotivationalTip] = useState('');
+  const [title, setTitle] = useState('');
 
   const saveMotivationalTip = (e) => {
     e.preventDefault();
     // Logic to handle saving the motivational tip to the database
-    // Use motivationalTip state to access entered tip and proceed accordingly
+    // Use motivationalTip and title state to access entered tip and title and proceed accordingly
     toast.success('Motivational tip saved successfully');
     // Additional logic for saving data to the database
   };
 
-  const onChange = (e) => {
+  const onChangeMotivationalTip = (e) => {
     e.persist();
     setMotivationalTip(e.target.value);
+  };
+
+  const onChangeTitle = (e) => {
+    e.persist();
+    setTitle(e.target.value);
   };
 
   return (
@@ -38,8 +36,12 @@ function DailyMotivationalTips() {
           <CCardBody>
             <CForm onSubmit={saveMotivationalTip}>
               <div className="mb-3">
+                <CFormLabel htmlFor='title'>Title</CFormLabel>
+                <Form.Control type="text" name="title" id="title" placeholder="Enter title" value={title} onChange={onChangeTitle} />
+              </div>
+              <div className="mb-3">
                 <CFormLabel htmlFor='motivationalTip'>Enter Tip</CFormLabel>
-                <Form.Control as="textarea" rows={3} name="motivationalTip" id="motivationalTip" placeholder="Enter your motivational tip" value={motivationalTip} onChange={onChange} />
+                <Form.Control as="textarea" rows={3} name="motivationalTip" id="motivationalTip" placeholder="Enter your motivational tip" value={motivationalTip} onChange={onChangeMotivationalTip} />
               </div>
                             
               <Button variant="primary" type="submit">
