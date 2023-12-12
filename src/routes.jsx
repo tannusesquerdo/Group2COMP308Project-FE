f6e703c723a09d174c6d54c9d455dfb177115e77
antonyyfk <109820108+antonyyfk@users.noreply.github.com>
2023-12-12 15:47:23 -0500

    Delete the unused route

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,12 +1,5 @@
 import React from 'react';
 
-const CreateStudent = React.lazy(() => import('./views/admin/CreateStudent'));
-const EditStudent = React.lazy(() => import('./views/admin/EditStudent'));
-const ListCourses = React.lazy(() => import('./views/ListCourses'));
-const CreateCourse = React.lazy(() => import('./views/admin/CreateCourse'));
-const ListStudents = React.lazy(() => import('./views/admin/ListStudents'));
-const AllCourses = React.lazy(() => import('./views/admin/ListCourses'));
-
 //Nurse
 const DailyMotivationalTips = React.lazy(() => import('./views/nurse/DailyMotivationalTips')); // Added on 12/04/2023 by TJ
 const VitalSignsInputForm = React.lazy(() => import('./views/nurse/VitalSignsInputForm')); // Added on 12/04/2023 by TJ
@@ -19,14 +12,6 @@ const SymptomsChecklist = React.lazy(() => import('./views/patient/SymptomsCheck
 
 const routes = [
   { path: '/', exact: true, name: 'Home' },
-  { path: '/courses', name: 'Courses', element: ListCourses },
-  { path: '/courses/my', name: 'My Courses', exact: true, element: ListCourses },
-  { path: '/admin/students/', name: 'All Students', element: ListStudents },
-  { path: '/admin/students/create', name: 'Add Students', element: CreateStudent },
-  { path: '/admin/students/:studentId/edit', name: 'Edit Student', element: EditStudent},
-  { path: '/admin/courses', name: 'All Courses', element: AllCourses },
-  { path: '/admin/courses/add', name: 'Create Course', element: CreateCourse },
-  { path: '/admin/courses/:courseId/edit', name: 'Edit Course', element: CreateCourse },
   //Nurse
   { path: '/nurse/daily-motivational-tips', name: 'Daily Motivational Tips', element: DailyMotivationalTips }, // Added on 12/04/2023 by TJ
   { path: '/nurse/vital-signs-input-form', name: 'Vital Signs Input Form', element: VitalSignsInputForm }, // Added on 12/04/2023 by TJ
