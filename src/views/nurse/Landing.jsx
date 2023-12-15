e55910401ae6ebed786c6f17f13ea24de984dd77
Tarique Jemison <jemisontarique@gmail.com>
2023-12-14 20:09:17 -0500

    landing page test

---
+++ src/views/nurse/Landing.jsx
@@ -0,0 +1,19 @@
+
+import React, { Component }  from 'react';
+//
+// this is the home page component
+function Landing(props)
+{
+
+
+    return (
+        <div>
+            <h2> Hold tight</h2>
+            <p>Will Update SOON....</p>
+        </div>
+    );
+
+}
+// withRouter will pass updated match, location, and history props 
+// to the wrapped component whenever it renders.
+export default Landing;
